1dcccd9c3499e7d5847beda22108d831b87c5120
Mike Huniewicz <michal.huniewicz@ft.com>
2015-07-30 18:17:39 +0100

    For all permutations.

--- all_repeats.py
+++ all_repeats.py
@@ -1,5 +1,7 @@
+import itertools
+
 with open("all_dna.txt") as f:
 	text = f.read()
 
-	word = "ACA"
-	print len([i for i, _ in enumerate(text) if text.startswith(word, i)])
+	for p in itertools.product("ACGT", repeat=5):
+		print "%s %d" % (''.join(p), len([i for i, _ in enumerate(text) if text.startswith(''.join(p), i)]))
