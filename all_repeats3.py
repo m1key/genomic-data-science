829916d3317e9132e1a3308ec3946a485d871775
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2015-08-01 12:44:42 +0100

    Added tests around finding DNA repeats.

--- all_repeats3.py
+++ all_repeats3.py
@@ -9,6 +9,7 @@ class Find:
 		return self.sequence == other.sequence and self.start_positions == other.start_positions
 	def __repr__(self):
 		return "Find('%s', %s)" % (self.sequence, self.start_positions)
+	__str__ = __repr__
 
 start = 4
 target = 12
@@ -18,6 +19,8 @@ assert Find("abc", [10]) != Find("abc", [11])
 assert Find("abc", [10]) != Find("abd", [10])
 assert Find("abc", [10, 15]) == Find("abc", [10, 15])
 assert Find("abc", [10, 15]) != Find("abc", [10, 16])
+find = Find("acgtgact", [10, 14, 19])
+assert eval(str(find)) == find
 
 def initial_repeats(dna):
 	start = 4
@@ -52,14 +55,48 @@ def find_repeats(dna, target, repeats):
 			all_candidates.extend(candidates)
 	return all_candidates
 
-print find_repeats(dna, 7, initial)
-
 def finds(x): return len(x.start_positions)
+
+def find_by_sequence(repeats, sequence, unique = False):
+	all_finds = [find for find in repeats if find.sequence == sequence]
+	if unique:
+		contains_one(repeats, sequence)
+		return all_finds[0]
+	else:
+		return all_finds
+def contains(repeats, sequence, times):
+	assert len(find_by_sequence(repeats, sequence)) == times
+def contains_one(repeats, sequence):
+	contains(repeats, sequence, 1)
+def contains_zero(repeats, sequence):
+	contains(repeats, sequence, 0)
+def has_start_positions(repeats, sequence, start_position_count):
+	assert len(find_by_sequence(repeats, sequence, unique = True).start_positions) == start_position_count
+def has_repeats_with_x_start_indeces(repeats, start_indeces_count, count):
+	assert len([find for find in repeats if len(find.start_positions) == start_indeces_count]) == count
+def has_start_indeces_at(repeats, sequence, start_indeces):
+	assert find_by_sequence(repeats, sequence, unique = True).start_positions == start_indeces
 
 with open("all_dna.txt") as f:
 	dna = f.read()
 	initial = initial_repeats(dna)
-	print max(map(finds, find_repeats(dna, 12, initial)))
+	repeats = find_repeats(dna, 12, initial)
+	contains_one(repeats, "AAAACGGCACCT") # Occurs twice in the dna.
+	contains_zero(repeats, "AAAACGGCTCGG") # Occurs once in the dna, so not a repeat.
+	contains_zero(repeats, "AAAACGGCTTAC") # Does not occur in the dna.
+	contains_one(repeats, "GCTGCTCGACGC") # Occurs three times in the dna.
+	has_start_positions(repeats, "AAAACGGCACCT", 2)
+	has_start_positions(repeats, "GCTGCTCGACGC", 3)
+	has_start_positions(repeats, "GTATCCCCGAAG", 2)
+	has_start_indeces_at(repeats, "TCGCGACACGTG", [10822, 39880])
+	has_start_indeces_at(repeats, "GCGATCGGCGCG", [8850, 12155, 44391])
+	assert len(repeats) == 921
+	has_repeats_with_x_start_indeces(repeats, 2, 886)
+	has_repeats_with_x_start_indeces(repeats, 3, 35)
+	has_repeats_with_x_start_indeces(repeats, 4, 0)
+	max_occurrences = max(map(finds, repeats))
+	assert max_occurrences == 3
+	print "All assertions passed."
 
 #	
 #	for p in itertools.product("ACGT", repeat=start):
